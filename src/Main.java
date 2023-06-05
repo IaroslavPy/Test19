0db3640cb50849440fb9f3d65aefb29709fede15
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-06-05 12:19:12 +0300

    Add test19

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        AtomicInteger counter = new AtomicInteger(0);
+        String[] arrayString = {"dsd", "ololo", "trulala"};
+        for (String obj: arrayString) {
+
+            System.out.println(counter.incrementAndGet() + ") " + obj);
+        }
+    }
+}
